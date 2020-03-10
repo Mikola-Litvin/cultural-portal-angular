e78c34494eeb9c5f7923bebb6dd125a852cfd301
Nikolai Shkrabkov <grr_by@mail.ru>
2020-03-11 00:46:09 +0300

    feat: add styleguide page and header modules

---
+++ src/app/styleguide/styleguide.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StyleguideRoutingModule } from './styleguide-routing.module';
+import { StyleguideComponent } from './styleguide.component';
+
+
+@NgModule({
+  declarations: [StyleguideComponent],
+  imports: [
+    CommonModule,
+    StyleguideRoutingModule
+  ]
+})
+export class StyleguideModule { }
