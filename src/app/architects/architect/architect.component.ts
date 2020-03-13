491e60aaf9f2837dfc89c8df4953419bb3985f30
Tatsiana Kharlamava <ustinya88@gmail.com>
2020-03-13 22:11:13 +0300

    feat: update architect component

--- src/app/architects/architect/architect.component.ts
+++ src/app/architects/architect/architect.component.ts
@@ -1,18 +1,75 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 import { pluck } from 'rxjs/operators';
+import {ContentfulService} from '../../servises/contentful.service';
+import { LanguageService } from 'src/app/servises/language.service';
 
 @Component({
   selector: 'app-architect',
   templateUrl: './architect.component.html',
   styleUrls: ['./architect.component.scss']
 })
-export class ArchitectComponent {
+export class ArchitectComponent implements OnInit{
 
   public architect = this.route.params.pipe(
     pluck('architectsId')
   );
 
-  constructor(private route: ActivatedRoute) { }
+  private architectsInfoID = 'eqqoo4gVdSVoohQvDxlHC';
+  public architectsInfo = this.contentfulService.getDataById(this.architectsInfoID);
 
+  public lang: string;
+  private responseData: object;
+  private firstEnterOnPage = true;
+  public architectsArr: object[] = [];
+
+  public architectDetails: any = {};
+  public architectId: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private contentfulService: ContentfulService,
+    private languageService: LanguageService,
+  ) {}
+
+  private getCurrentLangData(lang: string, responseData): void {
+    if (this.firstEnterOnPage) {
+      this.firstEnterOnPage = false;
+      return;
+    }
+    this.architectsArr.length = 0;
+    responseData.data.forEach(item => this.architectsArr.push(item));
+  }
+
+  private setArchitectDetails(){
+    // if(this.firstEnterOnPage){
+    //   this.firstEnterOnPage = false;
+    //   return
+    // }
+    this.architect.subscribe(
+      id => {
+        this.architectId = id;
+      }
+    );
+    // @ts-ignore
+    this.architectDetails = this.responseData.data.find(item => item.id === this.architectId);
+    console.log( 2);
+    console.log( this.architectDetails);
+  }
+
+  public ngOnInit(): void {
+    this.architectsInfo.subscribe(data => {
+      this.responseData = data;
+
+      this.languageService.language.subscribe(data => {
+        this.lang = data;
+        // this.getCurrentLangData(data, this.responseData);
+        this.setArchitectDetails();
+
+      });
+      console.log( 1);
+      console.log( this.responseData);
+      // this.setArchitectDetails();
+    });
+  }
 }
--- src/app/architects/architects.module.ts
+++ src/app/architects/architects.module.ts
@@ -9,10 +9,22 @@ import {LayoutModule} from '@angular/cdk/layout';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatOptionModule} from "@angular/material/core";
+import {MatSelectModule} from "@angular/material/select";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatRadioModule} from "@angular/material/radio";
+import {ReactiveFormsModule} from "@angular/forms";
+import { StepperComponent } from './stepper/stepper.component';
+import {MatInputModule} from "@angular/material/input";
+import {MatStepperModule} from "@angular/material/stepper";
+import {MatIconModule} from "@angular/material/icon";
+import {STEPPER_GLOBAL_OPTIONS} from "@angular/cdk/stepper";
+import {MatListModule} from "@angular/material/list";
 
 
 @NgModule({
-  declarations: [ArchitectsComponent, ArchitectComponent],
+  declarations: [ArchitectsComponent, ArchitectComponent, StepperComponent],
   imports: [
     CommonModule,
     ArchitectsRoutingModule,
@@ -20,12 +32,26 @@ import {MatButtonModule} from '@angular/material/button';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatRadioModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatStepperModule,
+    MatIconModule,
+    MatListModule,
   ],
   exports: [
     LayoutModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule
-  ]
+  ],
+  providers: [{
+    provide: STEPPER_GLOBAL_OPTIONS, useValue: {displayDefaultIndicatorType: false}
+  }]
 })
+
 export class ArchitectsModule { }
