2da6d7fa0b02cbd585f095ca2e97703ceea1f99d
Tatsiana Kharlamava <ustinya88@gmail.com>
2020-03-16 02:06:19 +0300

    feat: update architect and architects components

--- src/app/architects/architect/architect.component.ts
+++ src/app/architects/architect/architect.component.ts
@@ -1,15 +1,16 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import { AfterViewChecked, Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import { pluck } from 'rxjs/operators';
-import {ContentfulService} from '../../servises/contentful.service';
+import { ContentfulService } from '../../servises/contentful.service';
 import { LanguageService } from 'src/app/servises/language.service';
+import { DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-architect',
   templateUrl: './architect.component.html',
   styleUrls: ['./architect.component.scss']
 })
-export class ArchitectComponent implements OnInit{
+export class ArchitectComponent implements OnInit, AfterViewChecked {
 
   public architect = this.route.params.pipe(
     pluck('architectsId')
@@ -21,55 +22,112 @@ export class ArchitectComponent implements OnInit{
   public lang: string;
   private responseData: object;
   private firstEnterOnPage = true;
-  public architectsArr: object[] = [];
+  public arch: any;
 
-  public architectDetails: any = {};
   public architectId: string;
+  public lat: number;
+  public lng: number;
+  public zoom: number;
+  public safeSrc: SafeResourceUrl;
 
-  constructor(
+  public content: object= {
+    en: {
+      biography: 'Biography',
+      works: 'Works',
+      map: 'Map',
+      movie: 'Movie'
+    },
+    ru: {
+      biography: 'Биография',
+      works: 'Работы',
+      map: 'Карта',
+      movie: 'Видео'
+    },
+    by: {
+      biography: 'Біяграфія',
+      works: 'Работы',
+      map: 'Карта',
+      movie: 'Відэа'
+    }
+  };
+
+    constructor(
     private route: ActivatedRoute,
     private contentfulService: ContentfulService,
     private languageService: LanguageService,
-  ) {}
+    private sanitizer: DomSanitizer
+  ) {
+      this.safeSrc = '';
+  }
 
-  private getCurrentLangData(lang: string, responseData): void {
+  private setArchitectDetails(data) {
     if (this.firstEnterOnPage) {
       this.firstEnterOnPage = false;
       return;
     }
-    this.architectsArr.length = 0;
-    responseData.data.forEach(item => this.architectsArr.push(item));
-  }
-
-  private setArchitectDetails(){
-    // if(this.firstEnterOnPage){
-    //   this.firstEnterOnPage = false;
-    //   return
-    // }
     this.architect.subscribe(
       id => {
         this.architectId = id;
       }
     );
     // @ts-ignore
-    this.architectDetails = this.responseData.data.find(item => item.id === this.architectId);
-    console.log( 2);
-    console.log( this.architectDetails);
+    this.arch = data.data.find(item => item.id === this.architectId);
+    this.lat = +this.arch.coords.split(",")[0];
+    this.lng = +this.arch.coords.split(",")[1];
+    this.zoom = 8;
   }
 
   public ngOnInit(): void {
+    this.languageService.language.subscribe(data => {
+      this.lang = data;
+      this.setArchitectDetails(this.responseData);
+    });
+
     this.architectsInfo.subscribe(data => {
       this.responseData = data;
+      this.setArchitectDetails(data);
+    });
+  }
 
-      this.languageService.language.subscribe(data => {
-        this.lang = data;
-        // this.getCurrentLangData(data, this.responseData);
-        this.setArchitectDetails();
+  public ngAfterViewChecked(): void {
 
-      });
-      console.log( 1);
-      console.log( this.responseData);
-      // this.setArchitectDetails();
+    if (!document.querySelector('.modal-trigger')) return;
+
+    const body = document.body;
+    const modal = document.querySelector('.modal');
+    const trigger = document.querySelector('.modal-trigger');
+    const iframe = document.querySelector('.youtube');
+
+    trigger.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      const id = document.querySelector('.modal-trigger').getAttribute('data-id');
+      const src = `https://www.youtube.com/embed/${id}?autoplay=1&rel=0`;
+
+      iframe.setAttribute('src', src);
+      body.classList.add('noscroll');
+      modal.classList.add('show');
+    });
+
+    function closeModal(event) {
+      event.preventDefault();
+
+      body.classList.remove('noscroll');
+      modal.classList.remove('show');
+      iframe.setAttribute('src', '');
+    }
+
+    document.querySelector('.hide').addEventListener('click', (event) => {
+      closeModal(event);
+    });
+
+    body.addEventListener('keypress',function(event) {
+      if (event.keyCode === 27) {
+        closeModal(event);
+      }
     });
   }
 }
+
+
+
--- src/app/architects/architects.module.ts
+++ src/app/architects/architects.module.ts
@@ -9,22 +9,25 @@ import {LayoutModule} from '@angular/cdk/layout';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
-import {MatOptionModule} from "@angular/material/core";
-import {MatSelectModule} from "@angular/material/select";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatExpansionModule} from "@angular/material/expansion";
-import {MatRadioModule} from "@angular/material/radio";
-import {ReactiveFormsModule} from "@angular/forms";
-import { StepperComponent } from './stepper/stepper.component';
-import {MatInputModule} from "@angular/material/input";
-import {MatStepperModule} from "@angular/material/stepper";
-import {MatIconModule} from "@angular/material/icon";
-import {STEPPER_GLOBAL_OPTIONS} from "@angular/cdk/stepper";
-import {MatListModule} from "@angular/material/list";
+import {MatOptionModule} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatRadioModule} from '@angular/material/radio';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatIconModule} from '@angular/material/icon';
+import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MglTimelineModule} from 'angular-mgl-timeline.9';
+import {AngularYandexMapsModule} from 'angular8-yandex-maps';
+import {BoldDirective} from './directives/bold.directive';
 
 
 @NgModule({
-  declarations: [ArchitectsComponent, ArchitectComponent, StepperComponent],
+  declarations: [ArchitectsComponent, ArchitectComponent, BoldDirective],
   imports: [
     CommonModule,
     ArchitectsRoutingModule,
@@ -42,12 +45,19 @@ import {MatListModule} from "@angular/material/list";
     MatStepperModule,
     MatIconModule,
     MatListModule,
+    MatToolbarModule,
+    MglTimelineModule,
+    FormsModule,
+    AngularYandexMapsModule.forRoot (
+      '23b74637-3ecf-4417-8178-bb00a0411ee9')
   ],
+
   exports: [
     LayoutModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    BoldDirective
   ],
   providers: [{
     provide: STEPPER_GLOBAL_OPTIONS, useValue: {displayDefaultIndicatorType: false}
